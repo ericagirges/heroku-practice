4454f1018f0477ef4266c80c0a3521f02acaa756
Erica Girges <ericafiorenza@gmail.com>
2020-10-01 11:03:20 -0700

    add environment

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-    res.status(200).json({ ciao: "node 34"})
-})
+  res
+    .status(200)
+    .json({ ciao: "node 34", environment: process.env.NODE_ENV });
+});
 
 module.exports = server;
